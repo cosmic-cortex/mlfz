df1285edd63a277767b78d7e92ab7496ae7361b4
cosmic-cortex <theodore.danka@gmail.com>
2024-06-21 21:10:17 +0200

    matrix renamed back to tensor

--- tests/test_nn/test_tensor/test_tensor.py
+++ tests/test_nn/test_tensor/test_tensor.py
@@ -1,26 +1,26 @@
 import numpy as np
-from mlfz.nn.matrix import Matrix
+from mlfz.nn.tensor import Tensor
 
 
 def test_init():
     x_shape = (5, 4)
-    x = Matrix.from_random(*x_shape)
+    x = Tensor.from_random(*x_shape)
     assert x.shape == x_shape
 
-    x_ones = Matrix.ones_like(x)
+    x_ones = Tensor.ones_like(x)
     assert x_ones.shape == x_shape
 
-    x_zeros = Matrix.zeros_like(x)
+    x_zeros = Tensor.zeros_like(x)
     assert x_zeros.shape == x_shape
 
-    x_ones_from_np = Matrix.ones_like(x.value)
+    x_ones_from_np = Tensor.ones_like(x.value)
     assert x_ones_from_np.shape == x.shape
 
-    x_zeros_from_np = Matrix.zeros_like(x.value)
+    x_zeros_from_np = Tensor.zeros_like(x.value)
     assert x_zeros_from_np.shape == x.shape
 
 
 def test_add():
-    x, y = Matrix.from_random(5, 8), Matrix.from_random(5, 8)
+    x, y = Tensor.from_random(5, 8), Tensor.from_random(5, 8)
     z = x + y
     assert (z.value == x.value + y.value).all()
