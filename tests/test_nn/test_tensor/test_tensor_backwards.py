3c96618a6ea474aebf697540f7f00d0a8c21a043
cosmic-cortex <theodore.danka@gmail.com>
2024-06-27 21:26:54 +0200

    finite diff fixed

--- tests/test_nn/test_tensor/test_tensor_backwards.py
+++ tests/test_nn/test_tensor/test_tensor_backwards.py
@@ -3,8 +3,23 @@ from mlfz.nn.tensor import Tensor
 from functools import partial
 
 
-def _finite_diff(f, x, eps=1e-8):
-    return (f(x + eps) - f(x)) / eps
+# def _finite_diff(f, x, eps=1e-8):
+#     return (f(x + eps) - f(x)) / eps
+
+
+def _finite_diff(f, x, h=1e-8):
+    x = np.asarray(x, dtype=float)
+    grad = np.zeros_like(x)
+
+    for idx in np.ndindex(x.shape):
+        x_forward = x.copy()
+        x_backward = x.copy()
+        x_forward[idx] += h
+        x_backward[idx] -= h
+        print(x_forward.dtype, x_backward)
+        grad[idx] = (f(x_forward) - f(x_backward)) / (2 * h)
+
+    return grad.reshape(x.shape)
 
 
 def test_add():
@@ -12,12 +27,12 @@ def test_add():
     x = Tensor(np.array([[1, 2], [3, 4], [5, 6]]))
     y = Tensor(np.array([[7, 8], [9, 10], [11, 12]]))
 
-    f = lambda x, y: x + y
-    z = x + y
+    f = lambda x, y: (x + y).sum()
+    z = f(x, y)
     z.backward()
 
-    assert np.allclose(x.backwards_grad, _finite_diff(partial(f, y=y), x))
-    assert np.allclose(y.backwards_grad, _finite_diff(partial(f, x), y))
+    assert np.allclose(x.backwards_grad, _finite_diff(partial(f, y=y.value), x.value))
+    assert np.allclose(y.backwards_grad, _finite_diff(partial(f, x.value), y.value))
 
 
 def test_sub_pow():
@@ -25,9 +40,26 @@ def test_sub_pow():
     x = Tensor(np.array([[1, 2], [3, 4], [5, 6]]))
     y = Tensor(np.array([[7, 8], [9, 10], [11, 12]]))
 
-    f = lambda x, y: (x - y) ** 3
+    f = lambda x, y: ((x - y) ** 3).sum()
     z = f(x, y)
     z.backward()
 
-    assert np.allclose(x.backwards_grad, _finite_diff(partial(f, y=y), x))
-    assert np.allclose(y.backwards_grad, _finite_diff(partial(f, x), y))
+    assert np.allclose(x.backwards_grad, _finite_diff(partial(f, y=y.value), x.value))
+    assert np.allclose(y.backwards_grad, _finite_diff(partial(f, x.value), y.value))
+
+
+def test_reshape_sum():
+    x = Tensor(np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]]))
+
+    fs = [
+        lambda x: x.reshape(1, -1).sum(),
+        lambda x: x.reshape(-1).sum(),
+        lambda x: x.reshape(2, 6).sum(),
+        lambda x: x.reshape(6, 2).sum(),
+        lambda x: x.reshape(4, 3).sum(),
+    ]
+
+    for f in fs:
+        y = f(x)
+        y.backward()
+        assert np.allclose(y.backwards_grad, _finite_diff(f, x.value))
