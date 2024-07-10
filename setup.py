5f3970bc34ac1ce659b238c3f5d509beb11a53a3
cosmic-cortex <theodore.danka@gmail.com>
2024-07-10 04:07:34 +0200

    mlp page added

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlfz",
-    version="0.1.0",
+    version="0.1.1",
     author="Tivadar Danka",
     description="Machine Learning From Zero: an educational machine learning library.",
     long_description=open("README.md").read(),
