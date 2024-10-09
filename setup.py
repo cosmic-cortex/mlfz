34c41c66f8d8060a3b377cb512047cc977476b2c
cosmic-cortex <theodore.danka@gmail.com>
2024-10-09 20:18:04 +0200

    bump version number

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mlfz",
-    version="0.1.1",
+    version="0.1.2",
     author="Tivadar Danka",
     description="Machine Learning From Zero: an educational machine learning library.",
     long_description=open("README.md").read(),
